cdd58318d539a5406da8376197e5c0f1c50e21cc
uguremirmustafa <uguremirmustafa@gmail.com>
2021-05-13 16:49:29 +0300

    moved like icon next to language

--- components/SnippetPreviewCard.tsx
+++ components/SnippetPreviewCard.tsx
@@ -22,24 +22,29 @@ export default function Snippet({ snippet }) {
   const userLiked = true;
   return (
     <div className="snippet">
-      <div className="like">
-        <p>{count === 0 ? '' : count > 1 ? `${count} likes` : '1 like'}</p>
-
-        <svg viewBox="0 0 24 24" width="20" height="20" className={count > 0 ? 'liked' : ''}>
-          <path fill="none" d="M0 0H24V24H0z" />
-          <path d="M12.001 4.529c2.349-2.109 5.979-2.039 8.242.228 2.262 2.268 2.34 5.88.236 8.236l-8.48 8.492-8.478-8.492c-2.104-2.356-2.025-5.974.236-8.236 2.265-2.264 5.888-2.34 8.244-.228z" />
-        </svg>
-      </div>
       <div className="content">
         <h2 className="title">
           <Link href={`/snippet/${id}`}>{title}</Link>
         </h2>
         <div className="meta">
-          <p className="language">
-            Language:
-            <Link href={`/language/${slug}`}>{name}</Link>
-          </p>
+          <div className="meta__left">
+            <p className="language">
+              <svg viewBox="0 0 24 24">
+                <path fill="none" d="M0 0h24v24H0z" />
+                <path d="M23 12l-7.071 7.071-1.414-1.414L20.172 12l-5.657-5.657 1.414-1.414L23 12zM3.828 12l5.657 5.657-1.414 1.414L1 12l7.071-7.071 1.414 1.414L3.828 12z" />
+              </svg>
+              <Link href={`/language/${slug}`}>{name}</Link>
+            </p>
+            <div className="like">
+              <svg viewBox="0 0 24 24" width="20" height="20" className={count > 0 ? 'liked' : ''}>
+                <path fill="none" d="M0 0H24V24H0z" />
+                <path d="M12.001 4.529c2.349-2.109 5.979-2.039 8.242.228 2.262 2.268 2.34 5.88.236 8.236l-8.48 8.492-8.478-8.492c-2.104-2.356-2.025-5.974.236-8.236 2.265-2.264 5.888-2.34 8.244-.228z" />
+              </svg>
+              <p>{count === 0 ? '' : count > 1 ? `${count} likes` : '1 like'}</p>
+            </div>
+          </div>
           <div className="time">
+            <p>{timeAgo}</p>
             <p>
               {edited && (
                 <svg viewBox="0 0 24 24" width={16}>
@@ -48,7 +53,6 @@ export default function Snippet({ snippet }) {
                 </svg>
               )}
             </p>
-            <p>{timeAgo}</p>
           </div>
         </div>
         <p className="description">{description}</p>
